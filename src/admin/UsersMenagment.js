4f868e7003ae9f88b686e982bd250a320de516a4
E-zech <nemo557@gmail.com>
2023-11-22 11:47:11 +0200

    added

--- src/admin/UsersMenagment.js
+++ src/admin/UsersMenagment.js
@@ -5,6 +5,7 @@ import CheckBoxIcon from '@mui/icons-material/CheckBox';
 import DisabledByDefaultIcon from '@mui/icons-material/DisabledByDefault';
 import { GeneralContext } from '../App';
 import "./UsersMenagment.css";
+import { useMediaQuery } from "@mui/material";
 
 export default function UsersMenagment() {
   const [allClients, setAllClients] = useState([]);
@@ -27,47 +28,48 @@ export default function UsersMenagment() {
     }, [refresh]);
     
 
-  const columns = [
-    { field: 'id', headerName: 'ID', flex: 1 },
-    { field: 'firstName', headerName: 'First Name', flex: 1 },
-    { field: 'lastName', headerName: 'Last Name', flex: 1 },
-    { field: 'phone', headerName: 'Phone', flex: 1 },
-    { field: 'email', headerName: 'Email', flex: 1 },
-    { field: 'business', headerName: 'Business', flex: 1,
+    const isSmallScreen = useMediaQuery('(max-width:500px)');
 
-    renderCell: (params) => (
-    <div>
-      {params.row.business ? (
-        <CheckBoxIcon
-          onClick={(e) => {
-            e.stopPropagation();
-            handleBusiness(params.row);
-          }}
-          style={{
-            cursor: 'pointer',
-            color: 'green',
-          }}
-        />
-      ) : (
-        <DisabledByDefaultIcon
-          onClick={(e) => {
-            e.stopPropagation(); 
-            handleBusiness(params.row);
-          }}
-          style={{ cursor: 'pointer', color: 'red' }}/>
-      )}
-    </div>
-    ),
-    },
-
-    { field: 'delete' , headerName: 'Delete', flex: 1,
-      renderCell: (params) => (
-        <DeleteIcon
-          onClick={() => handleDelete(params.row.id)} // You need to implement handleDelete
-          style={{ cursor: 'pointer' }}/>
-      ),
-    }
-  ];
+    const columns = [
+      { field: 'id', headerName: 'ID', flex: 1 },
+      { field: 'firstName', headerName: 'First Name', flex: 1 },
+      // Conditionally render these columns based on screen width
+      !isSmallScreen && { field: 'lastName', headerName: 'Last Name', flex: 1 },
+      !isSmallScreen && { field: 'phone', headerName: 'Phone', flex: 1 },
+      !isSmallScreen && { field: 'email', headerName: 'Email', flex: 1 },
+      { field: 'business', headerName: 'Business', flex: 1,
+        renderCell: (params) => (
+          <div>
+            {params.row.business ? (
+              <CheckBoxIcon
+                onClick={(e) => {
+                  e.stopPropagation();
+                  handleBusiness(params.row);
+                }}
+                style={{
+                  cursor: 'pointer',
+                  color: 'green',
+                }}
+              />
+            ) : (
+              <DisabledByDefaultIcon
+                onClick={(e) => {
+                  e.stopPropagation(); 
+                  handleBusiness(params.row);
+                }}
+                style={{ cursor: 'pointer', color: 'red' }}/>
+            )}
+          </div>
+        ),
+      },
+      { field: 'delete', headerName: 'Delete', flex: 1,
+        renderCell: (params) => (
+          <DeleteIcon
+            onClick={() => handleDelete(params.row.id)} // You need to implement handleDelete
+            style={{ cursor: 'pointer' }}/>
+        ),
+      }
+    ].filter(Boolean);
 
 const handleBusiness = (client) => {
     setLoader(true);
@@ -110,21 +112,28 @@ const handleBusiness = (client) => {
     <>
     <header>
       <h1 className="main-title">User Table Management</h1>
-      <h3 className="sec-title">Here you can manage your clients, You can upgrade or delete them in the table below.</h3>
+      <h3 className="sec-title">Here you can manage your clients, <br/> You can upgrade or delete them in the table below.</h3>
+      
+      <h4 className="description-title">
+      If the CheckBox color is <span className="green">green</span> then the user is business.
+      <br/>
+      If it is <span className="red">red</span> the user is not business. 
+      
+      </h4>
+
     </header>
 
-    <section style={{ height: 'auto', width: '80vw', padding: '15px', margin:'0 auto' }}>
+    <section style={{ height: 'auto', width: '100%', padding: '15px', margin:'0 auto' }}>
       <DataGrid
         rows={allClients}
         columns={columns}
         pageSize={5}/>
     </section>
-
-    <h4 className="description-title">
-      If the CheckBox color is <span className="green">green</span>, then the user is business.
-      <br />
-      If it is <span className="red">red</span>, the user is not business.
-    </h4>
+    <h3 className="info-title">
+      Swipe ⬅️ or ➡️ for more info <br/> 
+      * In small size devices some of the info will be hidden *
+      </h3>
+ 
     </>
   );
 }
